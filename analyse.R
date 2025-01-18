ec78cd4cc515d0b27a009c32f76b12af04447d89
mariannehoule <marianne.houle.2@umontreal.ca>
2025-01-18 15:37:34 -0500

    Revert "ajouter transparence"
    This reverts commit 6d7c49e12388840b241614ef0f5d6164a0dd161a.

--- analyse.R
+++ analyse.R
@@ -4,7 +4,7 @@ library(ratadat)
 
 #Graphique 
 ggplot(data = complete_old, aes( x = weight, y = hindfoot_length)) +
-  geom_point(color = "blue", alpha = 0.2)
+  geom_point(color = "blue")
 
 #on doit d'abord sauvegarder la modification dans le fichier script ctrl s 
 
