6bc09f49591036647f1f244c62fe4a6128f40a63
mariannehoule <marianne.houle.2@umontreal.ca>
2025-01-18 15:29:26 -0500

    Ajout de commentaires

--- analyse.R
+++ analyse.R
@@ -5,3 +5,14 @@ library(ratadat)
 #Graphique 
 ggplot(data = complete_old, aes( x = weight, y = hindfoot_length)) +
   geom_point(color = "blue")
+
+#on doit d'abord sauvegarder la modification dans le fichier script ctrl s 
+
+# on doit add la modification 
+#git add analyse.R
+
+#on commit la modification
+#git commit -m "Modifier la couleur"
+
+#on peut vérifier le status 
+#git status 
